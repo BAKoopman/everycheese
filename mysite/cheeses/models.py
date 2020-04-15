42b491d4d746881f702c274e7b2da3ddaea269d4
bakoopman <brycek.3434@gmail.com>
2020-04-15 18:57:06 -0400

    Implemented cheese country of origin

--- mysite/cheeses/models.py
+++ mysite/cheeses/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
 from autoslug import AutoSlugField
+from django_countries.fields import CountryField
 
 class Cheese(TimeStampedModel):
     name = models.CharField("Name of Cheese", max_length=255)
     slug = AutoSlugField("Cheese Address", unique = True, always_update = False, populate_from = "name")
     description = models.TextField("Decription", blank = True)
+    country_of_origin = CountryField("Country of Origin", blank = True)
 
     class Firmness(models.TextChoices):
         UNSPECIFIED = "unspecified", "Unspecified"
@@ -13,7 +15,7 @@ class Cheese(TimeStampedModel):
         SEMI_SOFT = "semi-soft", "Semi-Soft"
         SEMI_HARD = "semi-hard", "Semi-Hard"
         HARD = "hard", "Hard"
-    #Should this line be indented?
+
     firmness = models.CharField("Firmness", max_length = 20, choices = Firmness.choices, default = Firmness.UNSPECIFIED)
     def __str__(self):
         return self.name
